b33051863c070503c8195f46bd20764c3c8486b7
Aidan Smith <83699315+shinysteel@users.noreply.github.com>
2026-06-29 18:25:58 +1000

    ShellFragment item. Palette renames. Recipe adjustments

--- Assets/Features/Items/ItemId.cs
+++ Assets/Features/Items/ItemId.cs
@@ -4,16 +4,17 @@ namespace NoMoreFishAndChips.Items
 {
     public enum ItemId
     {
-        None       = 0,
-        Driftwood  = 1,
-        Paddle     = 2,
-        Hammer     = 3,
-        Scrap      = 4,
-        Goop       = 5,
-        PalmSeed   = 6,
-        SharkFin   = 7,
-        FinBack    = 8,
-        ToothSpear = 9,
-        MakoTooth  = 10,
+        None          = 0,
+        Driftwood     = 1,
+        Paddle        = 2,
+        Hammer        = 3,
+        Scrap         = 4,
+        Goop          = 5,
+        PalmSeed      = 6,
+        SharkFin      = 7,
+        FinBack       = 8,
+        ToothSpear    = 9, 
+        MakoTooth     = 10,
+        ShellFragment = 11,
     }
 }
